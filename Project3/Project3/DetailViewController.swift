5329a8234a6b5c3997e59a8801d91dbeae1ed84a
Ammar Elshamy <ammar@corptia.com>
2019-10-30 19:31:00 +0200

    Third Project: Let users share and save their data by modifying project1 using UIActivityViewController and UIBarButtonItem

---
+++ Project3/Project3/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Ammar Elshamy on 10/29/19.
+//  Copyright © 2019 Ammar Elshamy. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    var selectedImage : String?
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        title = selectedImage
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("no image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
