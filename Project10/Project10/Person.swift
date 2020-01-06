16b262610718c969ee25718ec919986bcc68ea2a
Ammar Elshamy <ammar@corptia.com>
2020-01-06 04:46:14 +0200

    Names to Faces app using UICollectionView and the photo library

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Ammar Elshamy on 1/1/19.
+//  Copyright © 2019 Ammar Elshamy. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Ammar Elshamy on 1/1/19.
+//  Copyright © 2019 Ammar Elshamy. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
