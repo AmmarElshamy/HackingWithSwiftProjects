b1b277b84e9b5bd854c1f11f2ecbc8ee9e4b254c
Ammar Elshamy <ammar@corptia.com>
2020-01-14 07:50:49 +0200

    another simple game using SpriteKit

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,101 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Ammar Elshamy on 1/1/19.
+//  Copyright © 2019 Ammar Elshamy. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var starField: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    
+    var possibleEnimies = ["ball", "hammer", "tv"]
+    var gameTimer: Timer?
+    var isGameOver = false
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+        
+        physicsWorld.contactDelegate = self
+        physicsWorld.gravity = .zero
+        
+        starField = SKEmitterNode(fileNamed: "starField")
+        starField.position = CGPoint(x: 1024, y: 384)
+        starField.advanceSimulationTime(10)
+        starField.zPosition = -1
+        addChild(starField)
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        
+        score = 0
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+    }
+    
+    @objc func createEnemy() {
+        guard let enemy = possibleEnimies.randomElement() else { return }
+                
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
+        addChild(sprite)
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        for node in children {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+        
+        if !isGameOver {
+            score += 1
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        var location = touch.location(in: self)
+        
+        if location.y < 100 {
+            location.y = 100
+        } else if location.y > 668 {
+            location.y = 668
+        }
+        
+        player.position = location
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let explosion = SKEmitterNode(fileNamed: "explosion")!
+        explosion.position = player.position
+        addChild(explosion)
+        
+        player.removeFromParent()
+        isGameOver = true
+    }
+}
