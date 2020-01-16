e6bfdd981c96bbd9b255e902430362f8341e644f
Ammar Elshamy <ammar@corptia.com>
2020-01-16 04:20:08 +0200

    Safari extension using UITextView and NotificationCenter

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Ammar Elshamy on 1/16/20.
+//  Copyright © 2020 Ammar Elshamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
